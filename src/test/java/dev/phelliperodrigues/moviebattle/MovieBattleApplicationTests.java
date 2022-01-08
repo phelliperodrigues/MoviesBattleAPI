eafa77a09f555ecf5890e23418a52aa773dfea95
Phellipe Rodrigues <phellipe@pop-os.localdomain>
2022-01-07 22:05:39 -0300

    First commit - Creante project structure

---
+++ src/test/java/dev/phelliperodrigues/moviebattle/MovieBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phelliperodrigues.moviebattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBattleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
